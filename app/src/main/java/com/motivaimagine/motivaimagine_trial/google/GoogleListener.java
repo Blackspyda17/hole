88d442bff5ace277dcd60ae89b869758272375af
bl4k5pyd4 <paez1702@gmail.com>
2017-11-21 00:20:41 -0600

    AI-3.0 <bl4k5pyd4@bl4k5pyd4 Overwrite remote https://github.com/Blackspyda17/AndroidTEST-simpleWheaterAPP to local

--- app/src/main/java/com/motivaimagine/motivaimagine_trial/google/GoogleListener.java
+++
@@ -1,9 +0,0 @@
-package com.motivaimagine.motivaimagine_trial.google;
-
-public interface GoogleListener {
-  void onGoogleAuthSignIn(String authToken, String userId,String email,String lastname,String name,String url);
-
-  void onGoogleAuthSignInFailed(String errorMessage);
-
-  void onGoogleAuthSignOut();
-}
